4dfd082fe2a7f17311c93ea34d0ce2bd82f607c9
Brandon Beidel <bcbeidel@gmail.com>
2022-06-02 20:51:05 -0400

    fix: make users model db agnostic

--- models/marts/fct_users.sql
+++ models/marts/fct_users.sql
@@ -1,15 +1,12 @@
 with base as (
   select
+    case
+      when trim(lower(is_user_account)) in ('0', 'false') then 0
+      when trim(lower(is_user_account)) in ('1', 'true') then 1
+      else null
+    end as is_user_account,
     {{ dbt_utils.star(from=ref('stg_users'), except=["is_user_account"]) }}
-  , trim(lower(cast(is_user_account as string))) as is_user_account
   from {{ ref('stg_users') }}
 )
 
-select
-  * except(is_user_account)
-, case
-    when is_user_account in ('0', 'false') then 0
-    when is_user_account in ('1', 'true') then 1
-    else null
-  end as is_user_account
-from base
+select * from base
