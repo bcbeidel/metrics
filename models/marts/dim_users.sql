b968d6acd8f84178c9d49e4a6a6ff9bab62ff4b0
Brandon Beidel <bcbeidel@gmail.com>
2022-06-02 23:08:20 -0400

    allow for string 'no' and 'yes' to get around type issues

--- models/marts/dim_users.sql
+++ models/marts/dim_users.sql
@@ -1,8 +1,8 @@
 with base as (
   select
     case
-      when trim(lower(is_user_account)) in ('0', 'false') then 0
-      when trim(lower(is_user_account)) in ('1', 'true') then 1
+      when trim(lower(is_user_account)) in ('0', 'false', 'no') then 0
+      when trim(lower(is_user_account)) in ('1', 'true', 'yes') then 1
       else null
     end as is_user_account,
     {{ dbt_utils.star(from=ref('stg_users'), except=["is_user_account"]) }}
