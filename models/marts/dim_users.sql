723dc0f06d82370733028e0f2c9c279ad9d9d5c0
Brandon Beidel <bcbeidel@gmail.com>
2022-06-04 23:10:38 -0400

    remove trailing comma

--- models/marts/dim_users.sql
+++ models/marts/dim_users.sql
@@ -5,6 +5,5 @@
       when trim(lower(is_user_account)) in ('0', 'false', 'no') then false
       when trim(lower(is_user_account)) in ('1', 'true', 'yes') then true
       else null
-    end as is_user_account,
+    end as is_user_account
   from {{ ref('stg_users') }}
-  
